18af50fda70da4e8d3d4a4e0b40e5beae45dd927
Astle Sylvester Dsa <astlesylvesterdsa@Astles-MacBook-Air.local>
2024-07-22 04:57:50 +0530

    Basics

--- src/client.ts
+++ src/client.ts
@@ -1,1 +1,14 @@
+import PartySocket from "partysocket";
+
+let data: number = 2190;
+
+const ws = new PartySocket({
+  host: "localhost:1999", 
+});
+
+ws.send(JSON.stringify(data));
+ws.addEventListener('message', (e) => {
+  let res = JSON.parse(e.data);
+  console.log(`Modified number: ${res}`);
+})
 
